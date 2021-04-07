1277ef8e7e4316af25e167eb44652f5f8bcd20d4
wanghongyan01 <wanghongyan01@baidu.com>
2021-04-07 13:49:28 +0800

    add linkable ring signature

---
+++ advanced/link_ring_sign/README.md
@@ -0,0 +1,7 @@
+# Ring Signature
+Go implementation of linkable ring signature based on RSA
+
+## Tests
+```bash
+$ go test .
+```
---
+++ advanced/link_ring_sign/verify.go
@@ -0,0 +1,50 @@
+package link_ring_sign
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"math/big"
+)
+
+// verify
+func verify(sig, msg []byte) (bool, error) {
+	var ringSign LinkRingSignature
+	if err := json.Unmarshal(sig, &ringSign); err != nil {
+		return false, fmt.Errorf("failed to unmarshal signature, err: %v", err)
+	}
+
+	if err := checkSignature(ringSign); err != nil {
+		return false, err
+	}
+
+	key := sha256.Sum256(append(msg, ringSign.LinkKey...))
+	c, err := cky(ringSign.PublicKeys, ringSign.Xs, key[:], ringSign.V)
+	if err != nil {
+		return false, fmt.Errorf("failed to compute cky, err: %v", err)
+	}
+	if new(big.Int).SetBytes(c).Cmp(ringSign.V) != 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+// checkSignature check if signature is valid
+func checkSignature(sig LinkRingSignature) error {
+	if sig.V == nil {
+		return fmt.Errorf("invalid signature, nil v")
+	}
+	if sig.Xs == nil {
+		return fmt.Errorf("invalid signature, nil xs")
+	}
+	if sig.PublicKeys == nil {
+		return fmt.Errorf("invalid signature, nil public keys")
+	}
+	if sig.LinkKey == nil {
+		return fmt.Errorf("invalid signature, nil link key")
+	}
+	if len(sig.Xs) != len(sig.PublicKeys) {
+		return fmt.Errorf("invalid signature, public keys and xs should have same length")
+	}
+	return nil
+}
