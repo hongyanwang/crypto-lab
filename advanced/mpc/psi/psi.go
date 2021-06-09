fe0a414fef40345e8e76b119dc8c7bc5112fd844
wanghongyan01 <wanghongyan01@baidu.com>
2021-06-09 14:52:21 +0800

    add psi

--- advanced/mpc/psi/psi.go
+++ advanced/mpc/psi/psi.go
@@ -1,1 +1,80 @@
 package psi
+
+import (
+	"crypto/elliptic"
+	"math/big"
+)
+
+var (
+	DefaultCurveType = elliptic.P256()
+)
+
+type Point struct {
+	X *big.Int
+	Y *big.Int
+}
+
+// calOPRFRequest receiver calculates r*xG to hide secret
+func calOPRFRequest(secrets []*big.Int, r *big.Int) []Point {
+	var points []Point
+	for i := 0; i < len(secrets); i++ {
+		x, y := DefaultCurveType.ScalarBaseMult(secrets[i].Bytes())
+		x, y = DefaultCurveType.ScalarMult(x, y, r.Bytes())
+		point := Point{
+			X: x,
+			Y: y,
+		}
+		points = append(points, point)
+	}
+	return points
+}
+
+// calOPRFResponse sender calculates k*r*xG to hide k
+func calOPRFResponse(points []Point, secretK *big.Int) []Point {
+	var respPoints []Point
+	for i := 0; i < len(points); i++ {
+		x, y := DefaultCurveType.ScalarMult(points[i].X, points[i].Y, secretK.Bytes())
+		point := Point{
+			X: x,
+			Y: y,
+		}
+		respPoints = append(respPoints, point)
+	}
+	return respPoints
+}
+
+// recoverPRF receiver recovers k*xG(PRF) using r inverse
+func recoverPRF(points []Point, r *big.Int) []Point {
+	rInverse := new(big.Int).ModInverse(r, DefaultCurveType.Params().N)
+	var recoverPoints []Point
+	for i := 0; i < len(points); i++ {
+		x, y := DefaultCurveType.ScalarMult(points[i].X, points[i].Y, rInverse.Bytes())
+		point := Point{
+			X: x,
+			Y: y,
+		}
+		recoverPoints = append(recoverPoints, point)
+	}
+	return recoverPoints
+}
+
+// intersect get intersection of two sets
+func intersect(xps, yps []Point, ys []*big.Int) []*big.Int {
+	var intersection []*big.Int
+	for idx, yp := range yps {
+		if exist(yp, xps) {
+			intersection = append(intersection, ys[idx])
+		}
+	}
+	return intersection
+}
+
+// exist determine if a point is in a set
+func exist(p Point, points []Point) bool {
+	for _, point := range points {
+		if p.X.Cmp(point.X) == 0 {
+			return true
+		}
+	}
+	return false
+}
--- advanced/mpc/psi/psi_test.go
+++ advanced/mpc/psi/psi_test.go
@@ -1,1 +1,57 @@
 package psi
+
+import (
+	"crypto/rand"
+	"math/big"
+	"testing"
+)
+
+func TestPSI(t *testing.T) {
+	Xs := []*big.Int{big.NewInt(101), big.NewInt(202), big.NewInt(505), big.NewInt(808), big.NewInt(909), big.NewInt(1000)}
+	Ys := []*big.Int{big.NewInt(202), big.NewInt(303), big.NewInt(606), big.NewInt(808), big.NewInt(909)}
+	validIntersect := []*big.Int{big.NewInt(202), big.NewInt(808), big.NewInt(909)}
+
+	r, err := rand.Int(rand.Reader, DefaultCurveType.Params().N)
+	if err != nil {
+		t.Error(err)
+	}
+	secretK, err := rand.Int(rand.Reader, DefaultCurveType.Params().N)
+	if err != nil {
+		t.Error(err)
+	}
+
+	reqPoints := calOPRFRequest(Ys, r)
+	respPoints := calOPRFResponse(reqPoints, secretK)
+	recoverPoints := recoverPRF(respPoints, r)
+
+	// calculate k*xG directly
+	xPoints := calOPRFRequest(Xs, secretK)
+
+	// calculate intersection
+	intersection := intersect(xPoints, recoverPoints, Ys)
+
+	if !setsEqual(intersection, validIntersect) {
+		t.Errorf("OPRF test failed")
+	}
+}
+
+func setsEqual(s1, s2 []*big.Int) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for _, s := range s1 {
+		if !intExit(s, s2) {
+			return false
+		}
+	}
+	return true
+}
+
+func intExit(item *big.Int, set []*big.Int) bool {
+	for _, s := range set {
+		if item.Cmp(s) == 0 {
+			return true
+		}
+	}
+	return false
+}
