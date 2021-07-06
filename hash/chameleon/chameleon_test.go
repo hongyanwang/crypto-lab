83d9c2fb5cfe381d7aabfc7d9b2303a5d2db8ef6
wanghongyan01 <wanghongyan01@baidu.com>
2021-07-06 20:37:58 +0800

    chameleon test

--- hash/chameleon/chameleon_test.go
+++ hash/chameleon/chameleon_test.go
@@ -1,1 +1,27 @@
 package chameleon
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"math/big"
+	"testing"
+)
+
+func TestChamHash(t *testing.T) {
+	privkey, err := ecdsa.GenerateKey(DefaultCurveType, rand.Reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	msg := big.NewInt(123)
+	r := big.NewInt(456)
+	hash := ChamHash(msg, r, &privkey.PublicKey)
+
+	newMsg := big.NewInt(789)
+	newR := FindCollisionR(msg, newMsg, r, privkey.D)
+	newHash := ChamHash(newMsg, newR, &privkey.PublicKey)
+
+	if newHash.X.Cmp(hash.X) != 0 || newHash.X.Cmp(hash.X) != 0 {
+		t.Errorf("TestChamHash failed")
+	}
+}
